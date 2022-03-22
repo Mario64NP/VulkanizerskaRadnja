76600ce5294fac1d641cff04e1b97a79393a6559
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2022-03-22 18:10:32 +0100

    Dodata javadoc dokumentacija

--- VulkanizerskaRadnja/src/auto_radnja/gume/AutoGuma.java
+++ VulkanizerskaRadnja/src/auto_radnja/gume/AutoGuma.java
@@ -1,9 +1,28 @@
 package auto_radnja.gume;
 
+/**
+ * 
+ * Predstavlja jedan objekat gume za automobil.
+ * 
+ * @author Mario64NP
+ * @version 1.0
+ */
 public class AutoGuma {
+	/**
+	 * Porizvodjac i model gume
+	 */
 	private String markaModel = null;
+	/**
+	 * Precnik gume u cm
+	 */
 	private int precnik = -1;
+	/**
+	 * Sirina gume u cm
+	 */
 	private int sirina = -1;
+	/**
+	 * Visina gume u cm
+	 */
 	private int visina = -1;
 
 	public AutoGuma() {
@@ -17,10 +36,20 @@ public class AutoGuma {
 		setVisina(visina);
 	}
 
+	/**
+	 * Vraca proizvodjaca i model gume
+	 * @return proizvodjac i model gume (string)
+	 */
 	public String getMarkaModel() {
 		return markaModel;
 	}
 
+	/**
+	 * Postavlja proizvodjaca i model gume
+	 * @param markaModel naziv proizvodjaca i model gume
+	 * @throws NullPointerException ako je prosledjen null
+	 * @throws IllegalArgumentException ako prosledjeni naziv ima manje od 3 znaka
+	 */
 	public void setMarkaModel(String markaModel) {
 		if (markaModel == null)
 			throw new NullPointerException("Morate uneti marku i model");
@@ -29,42 +58,77 @@ public class AutoGuma {
 		this.markaModel = markaModel;
 	}
 
+	/**
+	 * Vraca precnik gume
+	 * @return precnik gume (int)
+	 */
 	public int getPrecnik() {
 		return precnik;
 	}
 
+	/**
+	 * Postavlja precnik gume
+	 * @param precnik precnik gume u cm
+	 * @throws IllegalArgumentException ako je uneti parametar van dozvoljenog opsega [13-22]
+	 */
 	public void setPrecnik(int precnik) {
 		if (precnik < 13 && precnik > 22)
 			throw new IllegalArgumentException("Precnik van opsega");
 		this.precnik = precnik;
 	}
 
+	/**
+	 * Vraca siriinu gume
+	 * @return sirina gume (int)
+	 */
 	public int getSirina() {
 		return sirina;
 	}
 
+	/**
+	 * Postavlja sirinu gume
+	 * @param sirina sirina gume u cm
+	 * @throws IllegalArgumentException ako je uneti parametar van dozvoljenog opsega [135-355]
+	 */
 	public void setSirina(int sirina) {
 		if (sirina < 135 && sirina > 355)
 			throw new IllegalArgumentException("Sirina van opsega");
 		this.sirina = sirina;
 	}
 
+	/**
+	 * Vraca visinu gume
+	 * @return visina gume (int)
+	 */
 	public int getVisina() {
 		return visina;
 	}
 
+	/**
+	 * Postavlja visinu gume
+	 * @param visina visina gume u cm
+	 * @throws IllegalArgumentException ako je uneti parametar van dozvoljenog opsega [25-95]
+	 */
 	public void setVisina(int visina) {
 		if (visina < 25 || visina > 95)
 			throw new IllegalArgumentException("Visina van opsega");
 		this.visina = visina;
 	}
 
+	/**
+	 * Predstavlja objekat kao string
+	 * @return String reprezentacija objekta
+	 */
 	@Override
 	public String toString() {
 		return "AutoGuma [markaModel=" + markaModel + ", precnik=" + precnik + ", sirina=" + sirina + ", visina="
 				+ visina + "]";
 	}
 
+	/**
+	 * Proverava da li je prosledjena guma jednaka trenutnoj
+	 * @return true ako su svi parametri jednaki kod obe gume; u suprotnom false
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
